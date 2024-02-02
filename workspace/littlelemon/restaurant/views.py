3da2f050fbe7f1a164fa18bf6c14daf6fcba90b2
Carlos Barajas <cabaraj2@uci.edu>
2024-02-02 13:40:55 -0800

    token authentication implemented

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
 def index(request):
@@ -9,13 +11,17 @@ def index(request):
 
 
 class MenuItemView(generics.ListCreateAPIView):
+    '''only registered users will be able to access to the menu'''
+    #permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    #permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
