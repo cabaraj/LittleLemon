dd92eaec1eba389f1f0397dd4ef38576a3892b22
Carlos Barajas <cabaraj2@uci.edu>
2024-02-03 13:14:54 -0800

    Booking Tests added

--- workspace/littlelemon/littlelemon/tests/test_models.py
+++ workspace/littlelemon/littlelemon/tests/test_models.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from restaurant.models import Menu, Booking
+from datetime import datetime
 
 class MenuItemTest(TestCase):
 
     def test_adding_item(self):
         item = Menu.objects.create(title="Ice Cream", price=80, inventory=100)
         self.assertEqual(str(item), "Ice Cream : 80")
+
+    def test_adding_booking(self):
+        booking = Booking.objects.create(name='Peter', guests=2, date=datetime(2024,2,14))
+        self.assertEqual(str(booking), "Peter (2)")
--- workspace/littlelemon/littlelemon/tests/test_views.py
+++ workspace/littlelemon/littlelemon/tests/test_views.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from restaurant.views import MenuItemView
-from restaurant.models import Menu
-from restaurant.serializers import MenuSerializer
+from restaurant.models import Menu, Booking
+from restaurant.serializers import MenuSerializer, BookingSerializer
 from decimal import Decimal
+from datetime import datetime
 
 class MenuViewTest(TestCase):
 
@@ -11,16 +12,28 @@ class MenuViewTest(TestCase):
         Menu.objects.create(title="Ice Cream", price=Decimal(80), inventory=100)
         Menu.objects.create(title="Raspado", price=Decimal(40), inventory=54)
 
-    def test_getall(self):
+        Booking.objects.create(name="Peter", guests=2, date=datetime(2024,2,14))
+
+
+    def test_menu_getall(self):
         # retrieve all the Menu object added
         # retrieve items must serialized
         testing_items = Menu.objects.all()
         self.assertEqual(2, len(testing_items))
 
-    def test_serializer_validation(self):
-
+    def test_menu_serializer_validation(self):
         items = Menu.objects.all()
         serialized_items = MenuSerializer(items, many=True)
         serialized_items = MenuSerializer(data=serialized_items.data, many=True)
         self.assertTrue(serialized_items.is_valid())
         
+
+    def test_booking_getall(self):
+        testing_items = Booking.objects.all()
+        self.assertEqual(1, len(testing_items))
+
+    def test_booking_serializer_validation(self):
+        items = Booking.objects.all()
+        serialized_items = BookingSerializer(items, many=True)
+        serialized_items = BookingSerializer(data=serialized_items.data, many=True)
+        self.assertTrue(serialized_items.is_valid())
